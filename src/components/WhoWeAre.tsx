b1e26b7521d56c14400eb226505fb80bc8b48e25
pritamthapa2043 <pritam.thapa2043@gmail.com>
2025-09-09 23:32:22 +0530

    (sections): Create who we are and techlongy sections

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -4,9 +4,24 @@ const WhoWeAre = () => {
   return (
     <section
       id="whoWeAre"
-      className="w-full h-screen flex items-center justify-center bg-gray-500 overflow-hidden"
+      className="w-full h-screen flex flex-col items-center justify-center bg-black overflow-hidden text-white px-6
+      bg-[url('/assets/images/whoweare-bg.webp')] bg-contain bg-center bg-no-repeat"
     >
-      <h2 className="text-4xl font-bold text-center">Who We Are</h2>
+      {/* Big Heading with line break */}
+      <h1 className="text-5xl md:text-8xl font-extrabold text-center mt-30 mb-30 leading-tight">
+        Future of Advanced <br /> Materials
+      </h1>
+
+      {/* Subtitle / Description */}
+      <p className="text-base md:text-lg text-center max-w-2xl mb-20">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+
+      {/* Button */}
+      <button className="bg-white text-gray-900 font-semibold px-6 py-3 rounded-2xl shadow-md hover:bg-gray-200 transition">
+        Get Started
+      </button>
     </section>
   );
 };
